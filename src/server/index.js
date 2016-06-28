2d6c9f88f78415ab4b5fb5ae3d465206f8f1ffff
Bob Corsaro <bob@embed.ly>
2016-06-28 13:28:07 -0400

    Packaging

--- src/server/index.js
+++ src/server/index.js
@@ -5,7 +5,13 @@
 import path from "path"
 import ChartedServer from './charted'
 import FileDb from "./db"
+import fs from "fs"
 
 let db = new FileDb(path.join(__dirname, '..', '..', '.charted_db'))
+
+if (process.env.PID) {
+  fs.writeFileSync(process.env.PID, process.pid)
+}
+
 ChartedServer.start(Number(process.env.PORT) || 3000, path.join(__dirname, '..', 'client'), db)
   .then((address: any) => console.log(`Running at ${address.address}:${address.port}`))
